d53acb6e0b7bac40a7dda63cff8ccf6ba7010dc7
Phil Wilkins <mp3monster@users.noreply.github.com>
2022-06-14 23:57:47 +0100

    Create ubuntu.sh

---
+++ OCI-setup/ubuntu.sh
@@ -0,0 +1,15 @@
+cd ..
+sudo apt install git
+sudo apt -y install openjdk-8-jdk
+java -version
+sudo apt install zip
+sudo curl -s get.sdkman.io | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install groovy
+groovy -version
+cd ~
+git clone https://github.com/mp3monster/LogGenerator
+chmod a+x LogGenerator/*.sh
+sudo apt install -y ruby
+sudo apt install -y rubygems
+sudo apt install -y ruby-dev
