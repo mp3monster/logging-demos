a0ba264934f1d2d6b3e33d44497c13bc9d5e7f25
Phil Wilkins <mp3monster@users.noreply.github.com>
2022-06-16 23:25:47 +0100

    updating to setup

--- OCI-setup/ubuntu.sh
+++ OCI-setup/ubuntu.sh
@@ -1,15 +1,29 @@
-cd ..
-sudo apt install git
-sudo apt -y install openjdk-8-jdk
+# parameter 1 is the slack token
+# parameter 2 is the IP of the other node in the demo
+cd ~
+
+echo 'export SLACK_TOKEN=$1' >> ~/.bashrc
+echo 'export OTHER_NODE=$2' >> ~/.bashrc
+
+sudo apt install -y make gcc
+sudo apt install -y ruby rubygems ruby-dev
+sudo gem install fluentd
+sudo gem install fluent-plugin-slack
+sudo gem install fluent-plugin-opensearch
+
+# sudo apt install git
+sudo apt install wget zip
+sudo apt install openjdk-8-jdk
 java -version
-sudo apt install zip
 sudo curl -s get.sdkman.io | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install groovy
 groovy -version
 cd ~
-git clone https://github.com/mp3monster/LogGenerator
-chmod a+x LogGenerator/*.sh
-sudo apt install -y ruby
-sudo apt install -y rubygems
-sudo apt install -y ruby-dev
+wget https://github.com/mp3monster/LogGenerator/archive/refs/heads/master.zip
+unzip master.zip
+rm master.zip
+chmod a+x LogGenerator*/*.sh
+
+
+# edit 
