8d75cbfab46e8ddacebfaec52af42285f815cf3c
Phil Wilkins <mp3monster@users.noreply.github.com>
2022-07-04 20:39:44 +0100

    Update log-generator1.sh
    correct case

--- Demo/log-generator1.sh
+++ Demo/log-generator1.sh
@@ -1,2 +1,2 @@
 source ./setLocations.sh
-groovy $logGenerator/logSimulator.groovy SimulatorConfig/basic-log-file.properties
+groovy $logGenerator/LogSimulator.groovy SimulatorConfig/basic-log-file.properties
