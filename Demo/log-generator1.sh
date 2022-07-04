80fc231db8c79c58005b66fdeb458b6226fa8c29
Phil Wilkins <mp3monster@users.noreply.github.com>
2022-07-04 20:15:37 +0100

    Update log-generator1.sh

--- Demo/log-generator1.sh
+++ Demo/log-generator1.sh
@@ -1,2 +1,2 @@
 source ./setLocations.sh
-groovy %logGenerator%/logSimulator.groovy SimulatorConfig/basic-log-file.properties
+groovy $logGenerator/logSimulator.groovy SimulatorConfig/basic-log-file.properties
